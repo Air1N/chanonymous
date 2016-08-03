c8d29a38e69e84a7f6f97d893438212e07d65edb
UnknownEvil <reapor12@hotmail.com>
2016-08-03 06:46:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
    connections++;
+   io.emit('connectionSend', connections);
    console.log('a user connected');
    socket.on('disconnect', function(){
        connections--;
