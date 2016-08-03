890843413353e854f42fadc9a04605a675d72dde
UnknownEvil <reapor12@hotmail.com>
2016-08-03 06:44:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 5000;
-
+var connections = 0;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+   connections++;
+   console.log('a user connected');
+   socket.on('disconnect', function(){
+       connections--;
+       console.log('user disconnected');
+   });
    socket.on('chat message', function(msg){
 	io.emit('chat message', msg);
    });
